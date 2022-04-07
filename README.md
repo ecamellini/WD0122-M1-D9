0b6122a1f1d5effebd1c01c93d66d1f70d14acba
Eric Camellini <eric.camellini@gmail.com>
2022-04-07 11:04:48 +0200

    Adding loops and structural types recap

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 1. Variables -- to store data, that can change overtime
 2. Data-types -- what can your data be?
    1. Basic data-types: string, nubmer, null, undefined, boolean
-   2. Structural data-types: arrays, objects
+   2. Structural data-types: arrays to store oredered collections of items, objects to represent a set of key-value properties
 3. Control flow -- choose what your program does and when
    1. If statement: choose if to execute some instructions or not, based on some condition
    2. Loops (while, for): execute the same instructions many times
---
+++ loops-structural-types-recap.js
@@ -0,0 +1,47 @@
+// Supppose that we are tracking shipments in our database.
+// We read the list of shipments that we have, so that
+// we can show it to the user.
+
+/* WHAT IS A SHIPMENT?
+- Origin: string
+- Destination: string
+- Weight: number (float)
+- Content: string
+- Delivered or not? boolean
+*/
+
+let exampleShipment = {
+  origin: "Milan",
+  destination: "Berlin",
+  weight: 5.35,
+  content: "Mozzarella",
+  isDelivered: false // Boolean, let's name it as a question
+}
+
+let shipments = [ // Maybe, in a real app, we read this list from a database
+  { origin: "Milan", destination: "Madrid", weight: 8.00, content: "Books", isDelivered: false},
+  { origin: "Brussels", destination: "Helsinky", weight: 4.00, content: "Pizzas", isDelivered: false},
+  { origin: "Bratislava", destination: "Berlin", weight: 7.40, content: "Fruit", isDelivered: true},
+  { origin: "Copenhagen", destination: "London", weight: 2.00, content: "LEGO blocks", isDelivered: false},
+]
+
+console.log(shipments)
+
+// All our shipments were delivered today.
+// We want to update the data so that it reflects the reality
+// isDelivered needs to be true.
+
+for (let index = 0; index < shipments.length; index++) {
+  let shipment = shipments[index]
+  shipment.isDelivered = true
+  console.log(`Shipment of ${shipment.content} was delivered.`)
+}
+
+console.log(shipments)
+
+
+// Variables exist ONLY INSIDE THE CODE BLOCK WHERE THEY ARE DECLARED
+// Variable scope/visiblility: if you exit a code block, its variables do not exist anymore.
+// console.log(shipment) // This instruction will throw an error
+// Also: variables are visible inside sub-blocks of the block you are into.
+// That's why we can see shipments variable inside the for loop.
